7a7fa52b8f98c56a972cd26c54b3d8881437b926
diaHelicoprion00 <reedishigamiwayne00@gmail.com>
2024-09-20 14:17:14 -0400

    refactor: removed _EntityXML from WikipediaAbstractsParser

--- tap_wikipedia/utils/wikipedia_abstracts_parser.py
+++ tap_wikipedia/utils/wikipedia_abstracts_parser.py
@@ -1,95 +1,65 @@
-from dataclasses import dataclass
-from pydantic import BaseModel
+from typing import Any
 from tap_wikipedia.models import wikipedia
-import xml.sax as sax
+from defusedxml import sax
 
 
 class WikipediaAbstractsParser(sax.ContentHandler):
-    """SAX Handler for Wikipedia Abstracts"""
-
-    @dataclass
-    class _EntityXML(BaseModel):
-        """A Pydantic Model to contain Wikipedia Abstracts"""
-
-        @dataclass
-        class AbstractInfo(BaseModel):
-            title: str = ""
-            abstract: str = ""
-            url: str = ""
-
-        @dataclass
-        class Sublink(BaseModel):
-            anchor: str = ""
-            link: str = ""
-
-        abstract_info: wikipedia.AbstractInfo = wikipedia.AbstractInfo()
-        sublinks: list[wikipedia.Sublink] = []
+    """SAX Handler for Wikipedia Abstracts."""
 
     def __init__(self):
-        self.currentData = ""
+        self._records: list[wikipedia.Record] = []
+        self.abstractInfo: wikipedia.AbstractInfo | None
         self.charBuffer: list[str] = []
-        self._records: list[WikipediaAbstractsParser._EntityXML] = []
-        self.abstractData = WikipediaAbstractsParser._EntityXML()
+        self.currentData: str
+        self.sublinks: list[wikipedia.Sublink] | None
 
     # reset character buffer and return all its contents as a string
-    def flushCharBuffer(self):
+    def flushCharBuffer(self) -> str:
         data = "".join(self.charBuffer)
         self.charBuffer = []
         return data.strip()
 
     # store individual records and reset abstracts dictionary
-    def storeRecord(self):
-        self._records.append(self.abstractData)
-        self.abstractData = WikipediaAbstractsParser._EntityXML()
+    def storeRecord(self) -> None:
+        self._records.append(
+            wikipedia.Record(
+                abstract_info=self.abstractInfo, sublinks=tuple(self.sublinks)
+            )
+        )
+        self.abstractInfo = None
+        self.sublinks = None
 
     # Call when an element starts
-    def startElement(self, tag, attributes):
+    def startElement(self, tag: str, attributes: Any) -> None:  # noqa: ARG002, ANN401
         self.currentData = tag
         if tag == "doc":
-            self.abstractData.abstract_info = (
-                WikipediaAbstractsParser._EntityXML().AbstractInfo()
-            )
-            self.abstractData.sublinks = []
+            self.abstractInfo = wikipedia.AbstractInfo(
+                title="", url="", abstract="")
+            self.sublinks = []
 
     # Call when an elements ends
-    def endElement(self, tag):
+    def endElement(self, tag: str) -> None:
         if tag == "title":
-            self.abstractData.abstract_info.title = self.flushCharBuffer()
+            self.abstractInfo.title = self.flushCharBuffer()
         elif tag == "url":
-            self.abstractData.abstract_info.url = self.flushCharBuffer()
+            self.abstractInfo.url = self.flushCharBuffer()
         elif tag == "abstract":
-            self.abstractData.abstract_info.abstract = (
-                self.flushCharBuffer()
-            )  # noqa: E501
+            self.abstractInfo.abstract = self.flushCharBuffer()
         elif tag == "anchor":
-            sublink = WikipediaAbstractsParser._EntityXML().Sublink()
+            sublink = wikipedia.Sublink()
             sublink.anchor = self.flushCharBuffer()
-            self.abstractData.sublinks.append(sublink)
+            self.sublinks.append(sublink)
         elif tag == "link":
-            self.abstractData.sublinks[-1].link = self.flushCharBuffer()
+            self.sublinks[-1].link = self.flushCharBuffer()
         elif tag == "doc":
             self.storeRecord()
 
     # store each chunk of character data within character buffer
-    def characters(self, content):
-        if self.currentData == "title":
-            self.charBuffer.append(content)
-        elif self.currentData == "url":
-            self.charBuffer.append(content)
-        elif self.currentData == "abstract":
-            self.charBuffer.append(content)
-        elif self.currentData == "anchor":
-            self.charBuffer.append(content)
-        elif self.currentData == "link":
+    def characters(self, content: str) -> None:
+        if self.currentData in ("title", "url", "abstract", "anchor", "link"):
             self.charBuffer.append(content)
 
     # return a tuple of wikipedia.Record
     @property
     def records(self) -> tuple[wikipedia.Record, ...]:
-        return tuple(
-            wikipedia.Record(
-                abstract_info=record.abstract_info, sublinks=tuple(
-                    record.sublinks)
-            )
-            for record in self._records
-        )
+        return tuple(self._records)
